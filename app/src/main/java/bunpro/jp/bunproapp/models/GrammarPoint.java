2824aaea3ed67ce08aa8963b099dcb821c8ebc81
zessirb <zessirb@gmail.com>
2019-04-18 08:41:23 +0200

    Fixing the switch tab crash

--- app/src/main/java/bunpro/jp/bunproapp/models/GrammarPoint.java
+++ app/src/main/java/bunpro/jp/bunproapp/models/GrammarPoint.java
@@ -1,5 +1,7 @@
 package bunpro.jp.bunproapp.models;
 
+import android.util.Log;
+
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,11 +26,19 @@ public class GrammarPoint {
     public static Comparator<GrammarPoint> levelComparator = new Comparator<GrammarPoint>() {
         @Override
         public int compare(GrammarPoint point1, GrammarPoint point2) {
-
-            String level1Str = point1.level.replaceAll("[^0-9]","");
-            String level2Str = point2.level.replaceAll("[^0-9]", "");
-            int number1 = Integer.parseInt(level1Str);
-            int number2 = Integer.parseInt(level2Str);
+            int number1, number2;
+            try {
+                String level1Str = point1.level.replaceAll("[^0-9]","");
+                number1 = Integer.parseInt(level1Str);
+            } catch (NumberFormatException e) {
+                number1 = 0;
+            }
+            try {
+                String level2Str = point2.level.replaceAll("[^0-9]", "");
+                number2 = Integer.parseInt(level2Str);
+            } catch (NumberFormatException e) {
+                number2 = 0;
+            }
 
             if (number1 != number2) {
                 if (number1 > number2) {
