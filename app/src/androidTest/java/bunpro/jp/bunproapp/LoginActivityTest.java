430bf90a8285b21c622935e7f67406c100413851
zessirb <zessirb@gmail.com>
2019-04-25 22:17:28 +0200

    Testing bitrise

--- app/src/androidTest/java/bunpro/jp/bunproapp/LoginActivityTest.java
+++ app/src/androidTest/java/bunpro/jp/bunproapp/LoginActivityTest.java
@@ -1,12 +1,9 @@
 package bunpro.jp.bunproapp;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
-import androidx.test.runner.lifecycle.Stage;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,11 +11,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.Collection;
-
 import bunpro.jp.bunproapp.activities.LoginActivity;
-import bunpro.jp.bunproapp.activities.MainActivity;
-import bunpro.jp.bunproapp.utils.EnvVarLoader;
 import bunpro.jp.bunproapp.utils.EspressoTestingIdlingResource;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -28,7 +21,6 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
