afb1c2b3c79b66fd1e9d3b05bbc8ab22a28fb0e0
Zessirb <zessirb@gmail.com>
2019-05-04 11:55:25 +0200

    Testing test env var secrets

--- app/src/main/java/bunpro/jp/bunproapp/utils/EnvVarLoader.java
+++ app/src/main/java/bunpro/jp/bunproapp/utils/EnvVarLoader.java
@@ -3,7 +3,7 @@ package bunpro.jp.bunproapp.utils;
 public class EnvVarLoader {
     static public String getEnvVar(String key) {
         if (System.getenv().containsKey(key)) {
-            return System.getenv().get(key)
+            return System.getenv().get(key);
         } else {
             return "";
         }
